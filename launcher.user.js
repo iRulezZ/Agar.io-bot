0d22bf986e78e9a0eea5eaf23cbc45a7f4f29a68
iRulezZ <erikeberding@web.de>
2015-06-13 13:13:00 +0200

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -884,7 +884,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  originalName = "NotReallyABot",
+  originalName = "FckPutin",
   sessionScore = 0,
   serverIP = "",
   interNodes = [],
